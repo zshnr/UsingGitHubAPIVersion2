53551fb58c3fc397903fcfcd15e1aa0bfe277de3
Stephen Giles <s.giles@hotmail.co.uk>
2014-10-27 01:08:53 +0000

    CSS and structure beginning to take shape.

--- src/interface.js
+++ src/interface.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
 
-		$('header').load(function () {
+		$('#headeruser').load(function () {
 			$(this).data('height', this.height);
 			}).bind('mouseenter', function(e) {
 			$(this).stop().animate({
 				height: $(this).data('height') * (e.type === 'mouseenter' ? 1.5 :  1)
 			  });
 			});
+
+		$('#getghusername').load(function () {
+			$('#headeruser').data('height', this.height);
+			}).bind('click', function(e) {
+			$('#headeruser').stop().animate({
+				height: $(this).data('height') * (e.type === 'click' ? 1 :  1)
+			  });
+			});
+
 });
